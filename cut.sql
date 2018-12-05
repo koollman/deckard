7b6f33b47a1d604199831cdeb254b2452d413944
Thomas Samson <tsamson@advanced-schema.com>
2018-12-05 18:47:47 +0100

    optimize

--- cut.sql
+++ cut.sql
@@ -7,3 +7,7 @@ DELETE FROM cards WHERE card_id IN (
 DELETE FROM cubes WHERE cube_id IN (
        SELECT cube_id FROM cube_card GROUP BY cube_id HAVING count(card_id) > 720 OR count(card_id) < 180
 );
+
+cluster;
+analyze;
+
--- post_load.sql
+++ post_load.sql
@@ -9,9 +9,18 @@ CREATE UNIQUE index cards_card_name_idx ON cards(card_name);
 CREATE index cube_card_card_id_idx ON cube_card(card_id);
 CREATE index cube_card_cube_id_idx ON cube_card(cube_id);
 
-CLUSTER cards using cards_pkey ;
-CLUSTER cubes using cubes_pkey ;
-CLUSTER cube_card using cube_card_card_id_idx;
+ALTER TABLE cards CLUSTER ON cards_pkey ;
+ALTER TABLE cubes CLUSTER ON cubes_pkey ;
+ALTER TABLE cube_card CLUSTER on cube_card_card_id_idx;
 
 ALTER TABLE cube_card ADD CONSTRAINT cube_card_cube_id_fkey FOREIGN KEY (cube_id) REFERENCES cubes(cube_id) ON DELETE CASCADE ON UPDATE CASCADE ;
 ALTER TABLE cube_card ADD CONSTRAINT cube_card_card_id_fkey FOREIGN KEY (card_id) REFERENCES cards(card_id) ON DELETE CASCADE ON UPDATE CASCADE ;
+
+CREATE STATISTICS cube_card_stat ON cube_id, card_id FROM cube_card;
+CREATE STATISTICS cards_stat ON card_id, card_name FROM cards;
+
+
+ALTER TABLE cube_card ALTER card_id SET STATISTICS 10000 ;
+ALTER TABLE cube_card ALTER cube_id SET STATISTICS 10000 ;
+
+analyze;
